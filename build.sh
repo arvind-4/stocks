6d443fa4c029b7db3758860b17e917a28fa4d458
Arvind <pingarvindforquestions@gmail.com>
2026-05-27 15:17:37 +0000

    fix(python-version): fixed the python version for the build files

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Upgrade pip..."
-python3.9 -m pip install --upgrade pip
+python3.12 -m pip install --upgrade pip
 
 echo "Installing dependencies..."
-python3.9 -m pip install -r requirements.txt
+python3.12 -m pip install -r requirements.txt
 
 echo "Collecting static files..."
-python3.9 manage.py collectstatic  --noinput --clear
+python3.12 manage.py collectstatic  --noinput --clear
