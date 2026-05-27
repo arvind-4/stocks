a603ee015c59c5a0bedd666606718184084959c1
Arvind <pingarvindforquestions@gmail.com>
2026-05-27 15:44:11 +0000

    ci(vercel): updated build command to install only prod deps

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Installing packages..."
-uv sync --locked
+uv sync --locked --no-dev
 
 echo "Collecting static files..."
 uv run python manage.py collectstatic  --noinput --clear
