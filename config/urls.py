89d20657d2e9aeebeaabd053acc82dfae0f9e742
Arvind <pingarvindforquestions@gmail.com>
2026-05-28 06:31:37 +0000

    fix(urls): updated urls in django

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,25 @@
 """config URL Configuration."""
 
-from django.urls import include, path
+from django.urls import path
+
+from stocks.views import (
+    about_view,
+    crypto_list_view,
+    crypto_view,
+    error_view,
+    home_page_view,
+    redirect_page_stocks,
+    redirect_price_view,
+    stocks_details,
+)
 
 urlpatterns = [
-    path("", include("stocks.urls")),
+    path("", home_page_view, name="home"),
+    path("cryto/", crypto_view, name="crypto"),
+    path("cryto/list/", crypto_list_view, name="crypto_list"),
+    path("stocks/details/", stocks_details, name="stocks_details"),
+    path("about/", about_view, name="about"),
+    path("error/", error_view, name="error"),
+    path("<stock_name>/", redirect_page_stocks, name="redirect_stock"),
+    path("stockdata/<stock_code>/", redirect_price_view, name="redirect_price"),
 ]
